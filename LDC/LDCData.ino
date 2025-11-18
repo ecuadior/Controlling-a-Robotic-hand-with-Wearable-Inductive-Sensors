10fe8d8f4ebaa2a72fcbaf0553cf9016db950ac5
ecuadior <john.guam.9000@gmail.com>
2025-11-18 10:15:53 -0500

    Replace terney operator for data

--- LDC/LDCData.ino
+++ LDC/LDCData.ino
@@ -1,5 +1,6 @@
 //Read LDC Sensor data
 //Use methods from RegisterSetup.ino
+//readChannel,setupI2C
 
 //store raw LDC value
 unsigned long data0, data1, data2, data3; 
@@ -7,15 +8,21 @@ unsigned long data0, data1, data2, data3;
 void RawData()
 {
     // update channel info and store data in vars
-    //previous group had 268435455 an arbitrary value appear frequecntly so tenery operator use
-    data0 = readChannel0() != 268435455 ? readChannel0() : data0;
-    data1 = readChannel1() != 268435455 ? readChannel1() : data1;
-    data2 = readChannel2() != 268435455 ? readChannel2() : data2;
-    data3 = readChannel3() != 268435455 ? readChannel3() : data3;
-    Serial.println("Channel 0: " , data0);
-    Serial.println("Channel 1: " , data1);
-    Serial.println("Channel 2: " , data2);
-    Serial.println("Channel 3: " , data3);
+    //previous group had 268435455 an arbitrary value appear frequecntly 
+    unsigned long d0 = readChannel0();
+    unsigned long d1 = readChannel1();
+    unsigned long d2 = readChannel2();
+    unsigned long d3 = readChannel3();
+    if(d0 != 268435455) data0 = d0;
+    if(d1 != 268435455) data1 = d1;
+    if(d2 != 268435455) data2 = d2;
+    if(d3 != 268435455) data3 = d3;
+
+    Serial.print("Channel 0: "); Serial.println(data0);
+    Serial.print("Channel 1: "); Serial.println(data1);
+    Serial.print("Channel 2: "); Serial.println(data2);
+    Serial.print("Channel 3: "); Serial.println(data3);
+    Serial.println();
 }
 //function for ploting raw data
 void plotRawData()
